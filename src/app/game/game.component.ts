efb27d73e3df1014fc69f667199a6528d984e7d2
Mario Markwart <mario.markwart@gmail.com>
2025-01-14 19:41:21 +0100

    game: card-stack

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,8 +1,9 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-game',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
