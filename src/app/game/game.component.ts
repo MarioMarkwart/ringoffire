ac462b312f223970d9cceadd1fb29bb6f9c2881e
Mario Markwart <mario.markwart@gmail.com>
2025-01-20 16:48:15 +0100

    OOP Model & shuffle 07

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, OnInit } from '@angular/core';
+import { Game } from '../../models/game';
 
 @Component({
   selector: 'app-game',
@@ -7,12 +8,28 @@ import { Component, Output, EventEmitter } from '@angular/core';
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
-export class GameComponent {
 
+export class GameComponent {
   pickCardAnimation = false;
+  game: Game;
+  // oder game!: Game; // non-null assertion operator
+
+  constructor() {
+    this.game = new Game;
+  }
+
+  ngOnInit():void {
+    this.newGame();
+  }
+
 
   @Output() public cardPicked = new EventEmitter<boolean>();
 
+  newGame() {
+    this.game = new Game;
+    console.log(this.game);
+
+  }
   takeCard() {
     this.pickCardAnimation = true;
   }
