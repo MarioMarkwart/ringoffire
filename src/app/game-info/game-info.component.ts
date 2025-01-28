de2b497d08e530bfbfeced6d90dddac1b240c9a0
Mario Markwart <mario.markwart@gmail.com>
2025-01-28 17:20:49 +0100

    game rules from JSON 18

--- src/app/game-info/game-info.component.ts
+++ src/app/game-info/game-info.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { MaterialModule } from '../shared/material.module';
+
 
 @Component({
   selector: 'app-game-info',
@@ -7,6 +8,39 @@ import { MaterialModule } from '../shared/material.module';
   templateUrl: './game-info.component.html',
   styleUrl: './game-info.component.scss'
 })
+
 export class GameInfoComponent {
 
+  cardActions = [
+    { title: 'Waterfall', description: 'Everyone has to start drinking at the same time. As soon as player 1 stops drinking, player 2 may stop drinking. Player 3 may stop as soon as player 2 stops drinking, and so on.' },
+    { title: 'You', description: 'You decide who drinks' },
+    { title: 'Me', description: 'Congrats! Drink a shot!' },
+    { title: 'Category', description: 'Come up with a category (e.g. Colors). Each player must enumerate one item from the category.' },
+    { title: 'Bust a jive', description: 'Player 1 makes a dance move. Player 2 repeats the dance move and adds a second one. ' },
+    { title: 'Chicks', description: 'All girls drink.' },
+    { title: 'Heaven', description: 'Put your hands up! The last player drinks!' },
+    { title: 'Mate', description: 'Pick a mate. Your mate must always drink when you drink and the other way around.' },
+    { title: 'Thumbmaster', description: 'Place your thumb on the table. Whenever you lift your thumb, everyone else must also lift their thumb. The last person to lift their thumb drinks.' },
+    { title: 'Men', description: 'All men drink.' },
+    { title: 'Quizmaster', description: 'You become the quizmaster. Ask a question to each player. If they answer incorrectly, they drink.' },
+    { title: 'Never have i ever...', description: 'Say something you never did. Everyone who did it has to drink.' },
+    { title: 'Rule', description: 'Make a rule. Everyone needs to drink when he breaks the rule.' },
+  ];
+
+  title: string = '';
+  description: string = '';
+
+  constructor() { }
+
+  @Input() card: string | undefined;
+
+  ngOnInit(): void { }
+
+  ngOnChanges(): void {
+    if (this.card) {
+      let cardNumber = Number(this.card?.split('_')[1]);
+      this.title = this.cardActions[cardNumber - 1].title;
+      this.description = this.cardActions[cardNumber - 1].description;
+    }
+  }
 }
